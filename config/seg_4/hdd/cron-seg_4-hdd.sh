f5ab51847897de9af08eaaf3d299a9061bae2275
name <vagrant@vagrant.box>
2014-10-27 22:04:43 -0400

    test cornmail

--- config/seg_4/hdd/cron-seg_4-hdd.sh
+++ config/seg_4/hdd/cron-seg_4-hdd.sh
@@ -5,3 +5,4 @@
 Dir='/deploy/mrtg/config/seg_4/hdd'
 env LANG=C /usr/bin/mrtg $Dir/192_168_4_47-nrt-hdd.cfg
 env LANG=C /usr/bin/mrtg $Dir/192_168_4_143-cmb01-hdd.cfg
+env LANG=C /usr/bin/mrtg $Dir/192_168_4_143-aaa-hdd.cfg
