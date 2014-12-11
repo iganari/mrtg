18f3fe7854592405e96583500470a2bc8dcc94bd
name <vagrant@vagrant.box>
2014-12-11 12:08:56 +0900

    add router of seg3

--- config/seg_3/cron-seg_3-router.sh
+++ config/seg_3/cron-seg_3-router.sh
@@ -7,13 +7,15 @@ Dir='/deploy/mrtg/config/seg_3'
 htmlDir='/deploy/mrtg/app/views/seg_3'
 
 # setting config-filename
-array=("192_168_3_3-aucfan-idcf-3"   # array[0]
-       "192_168_3_4-aucfan-idcf-4"   # array[1]
+array=("192_168_3_1-aucfan-idcf-1"   # array[0]
+       "192_168_3_2-aucfan-idcf-4"   # array[1]
+       "192_168_3_3-aucfan-idcf-3"   # array[2]
+       "192_168_3_4-aucfan-idcf-4"   # array[3]
        ) 
 
 
 num=0
-while [ $num -ne 2 ]                # array[ i + 1 ]
+while [ $num -ne 4 ]                # array[ i + 1 ]
   do     
     # for trafic
     env LANG=C /usr/bin/mrtg ${Dir}/tr/${array[${num}]}-tr.cfg
