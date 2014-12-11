3c73ccb24d60cd5d6195fc3db8223a8b9b11f621
name <vagrant@vagrant.box>
2014-12-11 14:19:18 +0900

    add ip server

---
+++ config/seg_2/cron-seg_2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+#
+# 2014/11/06
+
+# Difine directory
+Dir='/deploy/mrtg/config/seg_2'
+htmlDir='/deploy/mrtg/app/views/seg_2'
+
+# setting config-filename
+array=("192_168_2_159-mad"          # array[0]
+       "192_168_2_160-bcn"            # array[]
+       ) 
+
+
+num=0
+while [ $num -ne 2 ]                # array[ i + 1 ]
+  do     
+    # for trafic
+    env LANG=C /usr/bin/mrtg ${Dir}/tr/${array[${num}]}-tr.cfg
+    # for la
+    env LANG=C /usr/bin/mrtg ${Dir}/la/${array[${num}]}-la.cfg
+    # for mem
+    env LANG=C /usr/bin/mrtg ${Dir}/mem/${array[${num}]}-mem.cfg
+    # for hdd
+    env LANG=C /usr/bin/mrtg ${Dir}/hdd/${array[${num}]}-hdd.cfg
+    
+    num=`expr $num + 1`
+  done
+nkf -w --overwrite `find $htmlDir/ -name "*.html"`
+
+exit
