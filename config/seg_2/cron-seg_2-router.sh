417233a644f9850158bf72dcc27c7594bf569a35
name <vagrant@vagrant.box>
2014-12-25 13:01:14 +0900

    add router bitisle-13

--- config/seg_2/cron-seg_2-router.sh
+++ config/seg_2/cron-seg_2-router.sh
@@ -8,11 +8,12 @@ htmlDir='/deploy/mrtg/app/views/seg_2'
 
 # setting config-filename
 array=("192_168_2_14-afn-bitisle-14"   # array[0]
+       "192_168_2_13-afn-bitisle-13"   # array[0]
        ) 
 
 
 num=0
-while [ $num -ne 1 ]                # array[ i + 1 ]
+while [ $num -ne 2 ]                # array[ i + 1 ]
   do     
     # for trafic
     env LANG=C /usr/bin/mrtg ${Dir}/tr/${array[${num}]}-tr.cfg
