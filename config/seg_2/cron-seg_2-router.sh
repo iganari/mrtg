b80efc233bff1493e4929d78faa10cc393c82c49
name <vagrant@vagrant.box>
2015-01-01 09:13:12 +0900

    modify cron setting

--- config/seg_2/cron-seg_2-router.sh
+++ config/seg_2/cron-seg_2-router.sh
@@ -9,11 +9,13 @@ htmlDir='/deploy/mrtg/app/views/seg_2'
 # setting config-filename
 array=("192_168_2_14-afn-bitisle-14"   # array[0]
        "192_168_2_13-afn-bitisle-13"   # array[0]
+       "192_168_2_3-afn-bitisle-3"   # array[0]
+       "192_168_2_4-afn-bitisle-4"   # array[0]
        ) 
 
 
 num=0
-while [ $num -ne 2 ]                # array[ i + 1 ]
+while [ $num -ne 4 ]                # array[ i + 1 ]
   do     
     # for trafic
     env LANG=C /usr/bin/mrtg ${Dir}/tr/${array[${num}]}-tr.cfg
