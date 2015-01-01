0dbd3acc50805e3d9a8fc9b9f48d94f4ac032f3c
name <vagrant@vagrant.box>
2015-01-01 11:59:20 +0900

    add seg2 router

--- config/seg_2/cron-seg_2-router.sh
+++ config/seg_2/cron-seg_2-router.sh
@@ -9,13 +9,14 @@ htmlDir='/deploy/mrtg/app/views/seg_2'
 # setting config-filename
 array=("192_168_2_14-afn-bitisle-14"   # array[0]
        "192_168_2_13-afn-bitisle-13"   # array[0]
-       "192_168_2_3-afn-bitisle-3"   # array[0]
-       "192_168_2_4-afn-bitisle-4"   # array[0]
+       "192_168_2_3-afn-bitisle-3"     # array[0]
+       "192_168_2_4-afn-bitisle-4"     # array[0]
+       "192_168_2_1-afn-bitisle-1"     # array[4]
        ) 
 
 
 num=0
-while [ $num -ne 4 ]                # array[ i + 1 ]
+while [ $num -ne 5 ]                # array[ i + 1 ]
   do     
     # for trafic
     env LANG=C /usr/bin/mrtg ${Dir}/tr/${array[${num}]}-tr.cfg
